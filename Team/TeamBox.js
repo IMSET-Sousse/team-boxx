ad02a971108eda014a61b5e460c00b5d1bc2b559
Yasmina06-BD <yasminabendebba@gmail.com>
2024-12-03 04:57:04 +0100

    Adding JS

--- Team/TeamBox.js
+++ Team/TeamBox.js
@@ -1,3 +1,6 @@
+function download() {
+  alert("The app is coming soon! Stay tuned for updates from TeamBox team ! Thank you ");
+}
 document.addEventListener("DOMContentLoaded", () => {
   const featureCards = document.querySelectorAll(".feature-card");
   
@@ -14,3 +17,19 @@ document.addEventListener("DOMContentLoaded", () => {
   featureCards.forEach(card => observer.observe(card));
 });
 
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll(".functionality-card");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate-functionality");
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+
+  cards.forEach((card) => observer.observe(card));
+});
